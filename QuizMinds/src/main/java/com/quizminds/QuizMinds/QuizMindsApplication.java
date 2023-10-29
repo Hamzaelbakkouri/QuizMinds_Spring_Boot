a71f0fa1015bfa2292a01e49cd3d19d57c60464d
hamza elbakkouri <112905780+hamzaelbak66@users.noreply.github.com>
2023-10-29 22:16:19 +0100

    Spring Boot structure

---
+++ QuizMinds/src/main/java/com/quizminds/QuizMinds/QuizMindsApplication.java
@@ -0,0 +1,13 @@
+package com.quizminds.QuizMinds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizMindsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizMindsApplication.class, args);
+	}
+
+}
---
+++ QuizMinds/src/test/java/com/quizminds/QuizMinds/QuizMindsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quizminds.QuizMinds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizMindsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
